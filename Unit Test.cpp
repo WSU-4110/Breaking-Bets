e270f636311c01414c1fa7e5d2a95ba57021d244
fm2729 <90117796+fm2729@users.noreply.github.com>
2021-11-23 20:47:30 -0500

    Add files via upload

---
+++ Unit Test.cpp
@@ -0,0 +1,35 @@
+class PasswordConnection
+{
+	func saveUser(db *sql.DB, user *User) error {
+	if user.EmailAddress == "" {
+		return errors.New("User requires an email")
+	}
+	if len(user.Password) < 8 {
+		return errors.New("User password requires at least 8 characters")
+	}
+	hashedPassword, err = hash(user.Password)
+	if err != nil {
+		return err
+	}
+	_, err := db.Exec(`                                                                                                                          
+		INSERT INTO usr (password, email_address, created)                                                                                                                                                                                                                                                                                                                                                                                       
+		VALUES ($1, $2, $3);`,
+		hashedPassword, user.EmailAddress, time.Now(),
+	)
+	return err
+}
+
+	func saveUser(db *sql.DB, user *User) error {
+	err := validateUser(user)
+	if err != nil{
+		return err
+	}
+	user.Password, err = hash(user.Password)
+	if err != nil {
+		return err
+	}
+	return saveUserInDB(user)
+}
+
+
+}
